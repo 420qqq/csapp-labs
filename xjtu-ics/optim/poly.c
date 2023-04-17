dea3eaa394d88819fb83712b6024fb6d669b8b4b
420qqq <2593770256@qq.com>
2023-04-17 20:15:49 +0800

    optim

--- xjtu-ics/optim/poly.c
+++ xjtu-ics/optim/poly.c
@@ -4,19 +4,80 @@
 void poly_optim(const double a[], double x, long degree, double *result) {
     // your code here
     long i;
-    double t1, t2, t3, t4, t5, t6, t7;
+    double t1, t2, t3, t4, t5, t6, t7, t8, tp1, tp2;
     double r = a[degree];
-    for (i = degree - 1; i >= 4; i-=4) {
-        t4 = x * x * x * x;
-        t3 = x * x * x;
-        t2 = x * x;
-        t1 = x * a[i-2] + a[i-3];
-        t6 = t2 * a[i-1];
-        t5 = t3 * a[i];
-        t7 = t1 + t6;
-        r = t4 * r + t5;
+
+    t2 = x * x;
+    t3 = t2 * x;
+    t4 = t2 * t2;
+    t5 = t2 * t3;
+    t6 = t3 * t3;
+
+    for (i = degree - 1; i >= 6; i-=6) {
+        tp1 = a[i] * t5;
+        tp2 = a[i-1] * t4;
+        r = r * t6;
+        t1 = a[i-2] * t3;
+        t7 = a[i-3] * t2;
+        r += t1;
+        t8 = a[i-4] * x;
+        tp1 += tp2;
+        t7 += t8;
+        r += a[i-5];
+        r += tp1;
         r += t7;
     }
+    /*
+    t2 = x * x;
+    t3 = t2 * x;
+    t4 = t2 * t2;
+    t5 = t2 * t3;
+    t6 = t3 * t3;
+    t7 = t3 * t4;
+    t8 = t4 * t4;
+
+    for (i = degree - 1; i >= 8; i-=8) {
+        tp1 = a[i] * t7;
+        r = r * t8;
+        tp2 = a[i-1] * t6;
+        r += tp1;
+        t1 = a[i-3] * t4;
+        r += tp2;
+        tp1 = a[i-4] * t3;
+        r += t1;
+        tp2 = a[i-5] * t2;
+        r += tp1;
+        t1 = a[i-6] * x;
+        r += tp2;
+        tp2 += a[i-7];
+        r += t1;
+        r += tp2;
+    }*/
+
+    /*t2 = x * x;
+    t3 = t2 * x;
+    t4 = t2 * t2;
+
+    for (i = degree - 1; i >= 4; i-=4) {
+        t1 = x * a[i-2];
+        t6 = t2 * a[i-1];
+        r = t4 * r;
+        t5 = t3 * a[i];
+        t1 += a[i-3];
+        t1 += t6;
+        t1 += t5;
+        r += t1;
+    }
+    for (i = degree - 1; i >= 3; i-=3) {
+        t2 = x * x;
+        t1 = x * a[i-1];
+        t3 = x * t2;
+        t4 = t1 + a[i-2];
+        t5 = t2 * a[i];
+        r = r * t3;
+        t6 = t4 + t5;
+        r = r + t6;
+    }*/
     for (; i >= 0; --i) {
         r = a[i] + r * x;
     }
